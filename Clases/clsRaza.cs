25aa4f19c69bc55371cd5134f07ebd3c80963754
IsaBermudez <isabarboleda@yahoo.com>
2025-05-20 18:43:03 -0500

    Creación de la clase y controller raza

---
+++ Clases/clsRaza.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using VeterinariaServ.Models;
+
+namespace VeterinariaServ.Clases
+{
+    public class clsRaza
+    {
+        private dbVeterinariaEntities dbVeterinaria = new dbVeterinariaEntities();
+        public Raza raza { get; set; }
+
+
+        public string Insertar()
+        {
+            try
+            {
+                dbVeterinaria.Razas.Add(raza);
+                dbVeterinaria.SaveChanges();
+                return "Raza agregada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar una nueva raza: " + ex.Message;
+            }
+
+
+        }
+
+        public string Actualizar()
+        {
+            Raza raz = Consultar(raza.ID);
+            if (raz == null)
+            {
+                return "El nit de la raza no es válida";
+            }
+            dbVeterinaria.Razas.AddOrUpdate(raza);
+            dbVeterinaria.SaveChanges(); //
+            return "Se actualizó la raza correctamente";
+
+        }
+
+        public Raza Consultar(int id)
+        {
+            Raza raz = dbVeterinaria.Razas.FirstOrDefault(p => p.ID == id);
+            return raz;
+
+        }
+        public List<Raza> ConsultarTodos()
+        {
+            return dbVeterinaria.Razas
+                .OrderBy(p => p.Nombre)
+                .ToList();
+        }
+        public string EliminarXDocumento(int id)
+        {
+            try
+            {
+
+                Raza raz = Consultar(id);
+                if (raz == null)
+                {
+                    return "El nit de la raza no existe";
+                }
+                dbVeterinaria.Razas.Remove(raz);
+                dbVeterinaria.SaveChanges();
+                return "Se eliminó la raza correctamente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+    }
+}
---
+++ Controllers/RazasController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VeterinariaServ.Clases;
+using VeterinariaServ.Models;
+
+namespace VeterinariaServ.Controllers
+{
+    [RoutePrefix("api/Razas")]
+    public class RazasController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<Raza> ConsultarTodos()
+        {
+            clsRaza raz = new clsRaza();
+            return raz.ConsultarTodos();
+        }
+
+        [HttpGet]
+        [Route("ConsultarXNit")]
+        public Raza ConsultarXNit(int id)
+        {
+            clsRaza raz = new clsRaza();
+            return raz.Consultar(id);
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Raza raza)
+        {
+            clsRaza raz = new clsRaza();
+            raz.raza = raza;
+            return raz.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Raza raza)
+        {
+            clsRaza raz = new clsRaza();
+            raz.raza = raza;
+            return raz.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("EliminarXNit")]
+        public string EliminarXNit(int id)
+        {
+            clsRaza raz = new clsRaza();
+            return raz.EliminarXDocumento(id);
+        }
+    }
+}
